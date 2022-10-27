403b4b35f7b8800f5838a1005002317fad56d15c
poramin5202 <poramin5202@gmail.com>
2022-10-27 19:46:41 +0700

    edit name database in verify

--- pages/register/verify.vue
+++ pages/register/verify.vue
@@ -28,9 +28,9 @@ export default {
       next() {
           if(this.form.summinute != '0'){
            this.$store.dispatch('setTime', this.form)
-           this.$axios.patch(`https://projectbarber64-9435e-default-rtdb.asia-southeast1.firebasedatabase.app/user/${this.$store.getters.getLine.userId}/data.json`, this.$store.getters.getLine)
-           this.$axios.patch(`https://projectbarber64-9435e-default-rtdb.asia-southeast1.firebasedatabase.app/user/${this.$store.getters.getLine.userId}/data.json`, this.$store.getters.getTime)
-           this.$axios.patch(`https://projectbarber64-9435e-default-rtdb.asia-southeast1.firebasedatabase.app/user/${this.$store.getters.getLine.userId}/data.json`, this.$store.getters.getDatahair).then((res) => {
+           this.$axios.patch(`https://projectbarber64-9435e-default-rtdb.asia-southeast1.firebasedatabase.app/userLineliff/${this.$store.getters.getLine.userId}/data.json`, this.$store.getters.getLine)
+           this.$axios.patch(`https://projectbarber64-9435e-default-rtdb.asia-southeast1.firebasedatabase.app/userLineliff/${this.$store.getters.getLine.userId}/data.json`, this.$store.getters.getTime)
+           this.$axios.patch(`https://projectbarber64-9435e-default-rtdb.asia-southeast1.firebasedatabase.app/userLineliff/${this.$store.getters.getLine.userId}/data.json`, this.$store.getters.getDatahair).then((res) => {
             this.$router.push("/register/step4")
            })
          
